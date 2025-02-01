e9a327a045b1697b0e1eac3cea19b72796d71a51
anonymousProgrammer53 <aprogrammer53@gmail.com>
2025-02-02 02:57:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ fetch('https://www.omdbapi.com/?apikey=3bbc8b76&s=One Piece')
     .then(data => {
         let cards = '';
         data.Search.forEach((movie) => {
-            cards += `<div class="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-3 my-4">
+            cards += `<div class="col-6 col-sm-6 col-md-4 col-lg-4 col-xl-3 my-4">
                         <div class="card">
                             <img src="${movie.Poster}" class="card-img-top">
                             <div class="card-body">
-                                <h5 class="card-title">${movie.Title}</h5>
-                                <h6 class="card-subtitle mb-2 text-muted">${movie.Year}</h6>
-                                <p>${movie.Type}</p>
-                                <button class="btn btn-primary modal-detail-button" data-bs-toggle="modal" data-bs-target="#showMovieDetailModal" data-imdbid=${movie.imdbID}>Show Movie Detail</button>
+                                <h4 class="card-title">${movie.Title}</h4>
+                                <h4 class="card-subtitle mb-2 text-muted">${movie.Year}</h4>
+                                <h4 class="card-text mb-2">${movie.Type}</h4>
+                                <button class="fs-4 btn btn-lg btn-primary modal-detail-button" data-bs-toggle="modal" data-bs-target="#showMovieDetailModal" data-imdbid=${movie.imdbID}>Show Movie Detail</button>
                             </div>
                         </div>
                     </div>`});
@@ -37,7 +37,6 @@ fetch('https://www.omdbapi.com/?apikey=3bbc8b76&s=One Piece')
                         <div class="modal-dialog modal-lg">
                             <div class="modal-content">
                                 <div class="modal-header">
-                                    <h1 class="modal-title fs-5" id="showMovieDetailModalLabel"><strong>Title</strong> : ${data.Title}</h1>
                                     <button type="button" class="btn-close" data-bs-dismiss="modal"
                                         aria-label="Close"></button>
                                 </div>
@@ -45,6 +44,7 @@ fetch('https://www.omdbapi.com/?apikey=3bbc8b76&s=One Piece')
                                     <div class="row">
                                         <div class="col-12">
                                             <ul class="list-group list-group-flush">
+                                                <li class="list-group-item"><strong>Title</strong> : ${data.Title}</li>
                                                 <li class="list-group-item"><strong>Year</strong> : ${data.Year}</li>
                                                 <li class="list-group-item"><strong>Type</strong> : ${data.Type}</li>
                                                 <li class="list-group-item"><strong>Released</strong> : ${data.Released}</li>
