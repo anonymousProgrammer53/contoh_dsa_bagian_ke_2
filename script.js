d36ef5036c5aef66448c0f972f2ddc4466b65320
anonymousProgrammer53 <aprogrammer53@gmail.com>
2025-04-02 21:00:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,136 +1,120 @@
-const componentMoviesContainer = () => {
+const getMoviesContainer = () => {
     return document.getElementById('movies-container');
 }
 
-const componentInputGroupSizingLg = () => {
-    return document.getElementById('inputGroup-sizing-lg');
+const searchInput = () => {
+    return document.getElementById('search-input');
 }
 
-const componentDetailMoviesContainer = () => {
-    return document.getElementById('movies-detail-container');
+const fetchData = (url, callback) => {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            callback(data);
+        })
+        .catch(error => console.error('Error:', error));
 }
 
-const buttonsShowDetail = () => {
-    return document.querySelectorAll('button[data-imdbid]');
-}
-
-const componentMoviesContainerColl = (parentComponent) => {
+const createCardGroup = (parentComponent, data) => {
     const div = document.createElement('div');
-    div.classList.add('col-sm-4', 'col-6');
+    div.classList.add('row', 'row-cols-2', 'row-cols-sm-3', 'g-4');
+
+    data.forEach(item => {
+        const card = document.createElement('div');
+        card.classList.add('col');
+        card.innerHTML = `
+            <div class="card">
+                <img src="${item.Poster !== 'N/A' ? item.Poster : 'placeholder.jpg'}" class="card-img-top" alt="${item.Title}">
+                <div class="card-body p-2">
+                    <h6 class="card-title m-0">${item.Title}</h6>
+                    <small class="card-text text-muted mb-2 d-block">Tahun: ${item.Year}</small>
+                    <button class="btn btn-primary d-block w-100" data-imdbid=${item.imdbID}>Selengkapnya</button>
+                </div>
+            </div>
+        `;
+        div.appendChild(card);
+
+        const cardButtons = card.querySelectorAll('button');
+        cardButtons.forEach(button => {
+            button.addEventListener('click', () => {
+                const getModalFade = document.getElementById('modalFade');
+                if (getModalFade) {
+                    getModalFade.remove();
+                }
+                fetchData(`https://www.omdbapi.com/?apikey=3bbc8b76&i=${button.getAttribute('data-imdbid')}`, (data) => {
+                    if (data) {
+                        const parentComponent = document.getElementById('movies-detail-container');
+                        createModalFade(parentComponent, data);
+                    }
+                });
+                if (getModalFade) {
+                    const modalFade = new bootstrap.Modal(getModalFade);
+                    modalFade.show();
+                }
+            });
+        });
+    });
+
     parentComponent.appendChild(div);
-    return div;
 }
 
-const componentCard = (parentComponent, poster, title, year, imdbID) => {
-    const div = document.createElement('div');
-    div.classList.add('card');
-    div.innerHTML = `
-        <img src="${poster}" class="card-img-top" alt="${title}">
-        <div class="card-body p-2">
-            <h6 class="card-title m-0 p-0">Judul : ${title}</h6>
-            <p class="card-text text-secondary mb-2">Tahun : ${year}</p>
-            <button class="btn btn-primary d-block w-100" data-imdbid=${imdbID} data-bs-toggle="modal" data-bs-target="#staticBackdrop">Selengkapnya</button>
-        </div>`;
-    parentComponent.appendChild(div);
-    return div;
-}
-
-const componentModal = (parentComponent, { title, year, writer, genre, actors, runtime, language, country, released }) => {
+const createModalFade = (parentComponent, data) => {
     const div = document.createElement('div');
     div.classList.add('modal', 'fade');
-    div.setAttribute('id', 'staticBackdrop');
-    div.setAttribute('data-bs-keyboard', 'false');
-    div.setAttribute('data-bs-keyboard', 'false');
+    div.setAttribute('id', 'modalFade');
     div.setAttribute('tabindex', '-1');
-    div.setAttribute('aria-labelledby', 'staticBackdropLabel');
-    div.setAttribute('aria-hidden', 'true"');
     div.innerHTML = `
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <h1 class="modal-title fs-5" id="staticBackdropLabel">Informasi Lengkap</h1>
+                <h5 class="modal-title">Informasi lebih lengkap</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item"><strong>Judul :&emsp;</strong>${title}</li>
-                    <li class="list-group-item"><strong>Tahun :&emsp;</strong> ${year}</li>
-                    <li class="list-group-item"><strong>Penulis :&emsp;</strong> ${writer}</li>
-                    <li class="list-group-item"><strong>Genre :&emsp;</strong> ${genre}</li>
-                    <li class="list-group-item"><strong>Pemeran :&emsp;</strong> ${actors}</li>
-                    <li class="list-group-item"><strong>Durasi :&emsp;</strong> ${runtime}</li>
-                    <li class="list-group-item"><strong>Bahasa :&emsp;</strong> ${language}</li>
-                    <li class="list-group-item"><strong>Negara :&emsp;</strong> ${country}</li>
-                    <li class="list-group-item"><strong>Tanggal Rilis :&emsp;</strong> ${released}</li>
+                    <li class="list-group-item"><strong>Judul :&emsp;</strong>${data.Title}</li>
+                    <li class="list-group-item"><strong>Tahun :&emsp;</strong>${data.Year}</li>
+                    <li class="list-group-item"><strong>Genre :&emsp;</strong>${data.Genre}</li>
+                    <li class="list-group-item"><strong>Sutradara :&emsp;</strong>${data.Director}</li>
+                    <li class="list-group-item"><strong>Penulis :&emsp;</strong>${data.Writer}</li>
+                    <li class="list-group-item"><strong>Pemeran :&emsp;</strong>${data.Actors}</li>
+                    <li class="list-group-item"><strong>Durasi :&emsp;</strong>${data.Runtime}</li>
+                    <li class="list-group-item"><strong>IMDb Rating :&emsp;</strong>${data.imdbRating}</li>
+                    <li class="list-group-item"><strong>IMDb Votes :&emsp;</strong>(${data.imdbVotes})</li>
+                    <li class="list-group-item"><strong>Metascore :&emsp;</strong>${data.Metascore}</li>
+                    <li class="list-group-item"><strong>Penghargaan :&emsp;</strong>${data.Awards}</li>
+                    <li class="list-group-item"><strong>Box Office :&emsp;</strong>${data.BoxOffice}</li>
+                    <li class="list-group-item"><strong>Tanggal Rilis :&emsp;</strong>${data.Released}</li>
+                    <li class="list-group-item"><strong>Negara :&emsp;</strong>${data.Country}</li>
+                    <li class="list-group-item"><strong>Bahasa :&emsp;</strong>${data.Language}</li>
+                    <li class="list-group-item"><strong>Rating Usia :&emsp;</strong>${data.Rated}</li>
+                    <li class="list-group-item"><strong>IMDb ID :&emsp;</strong>${data.imdbID}</li>
+                    <li class="list-group-item"><strong>Jenis :&emsp;</strong>${data.Type}</li>
+                    <li class="list-group-item"><strong>Sinopsis :&emsp;</strong>${data.Plot}</li>
                 </ul>
             </div>
         </div>
-    </div>
-    `;
+    </div>`;
+
     parentComponent.appendChild(div);
-    return div;
-}
-
-const displayComponentCard = (url) => {
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            if (data.Search) {
-                data.Search.forEach(item => {
-                    const colDiv = componentMoviesContainerColl(componentMoviesContainer());
-                    const card = componentCard(colDiv, item.Poster, item.Title, item.Year, item.imdbID);
-
-                    card.querySelector('button').addEventListener('click', (event) => {
-                        const imdbID = event.target.getAttribute('data-imdbid');
-                        displayComponentModal(`https://www.omdbapi.com/?apikey=3bbc8b76&i=${imdbID}`);
-                    });
-                });
-            } else {
-                console.error("No results found.");
-            }
-        })
-        .catch(error => console.error('Error:', error));
 };
 
-const displayComponentModal = (url) => {
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            if (data) {
-                console.log(data);
-                const detailContainer = componentDetailMoviesContainer();
-                detailContainer.innerHTML = '';
-                componentModal(detailContainer, {
-                    title: data.Title,
-                    year: data.Year,
-                    writer: data.Writer,
-                    genre: data.Genre,
-                    actors: data.Actors,
-                    runtime: data.Runtime,
-                    language: data.Language,
-                    country: data.Country,
-                    released: data.Released,
-                });
-
-            } else {
-                console.error("No results found.");
-            }
-        })
-        .catch(error => console.error('Error:', error));
-};
-
-componentInputGroupSizingLg().addEventListener('click', () => {
-    componentMoviesContainer().innerHTML = ``;
-    let inputValue = componentInputGroupSizingLg().nextElementSibling.value.trim();
-    let words = inputValue.split(/\s+/).slice(0, 2);
-    words.forEach(word => {
-        if (word) {
-            displayComponentCard(`https://www.omdbapi.com/?apikey=3bbc8b76&s=${word}`);
+searchInput().addEventListener('click', () => {
+    getMoviesContainer().innerHTML = ``;
+    const inputValue = searchInput().nextElementSibling.value;
+    fetchData(`https://www.omdbapi.com/?apikey=3bbc8b76&s=${inputValue}`, (data) => {
+        if (data && data.Search) {
+            const parentComponent = getMoviesContainer();
+            createCardGroup(parentComponent, data.Search);
         }
     });
 });
 
 document.addEventListener('DOMContentLoaded', () => {
-    displayComponentCard(`https://www.omdbapi.com/?apikey=3bbc8b76&s=One`);
-    displayComponentCard(`https://www.omdbapi.com/?apikey=3bbc8b76&s=Piece`);
+    fetchData('https://www.omdbapi.com/?apikey=3bbc8b76&s=One', (data) => {
+        if (data && data.Search) {
+            const parentComponent = getMoviesContainer();
+            createCardGroup(parentComponent, data.Search);
+        }
+    });
 });
